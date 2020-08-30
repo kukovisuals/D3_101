3c2338c19458f24a08c74071d048b96ec0406442
kukovisuals <clubhousenet@live.com>
2020-08-29 22:32:50 -0400

    drawing circles with data

--- text_1/index.js
+++ text_1/index.js
@@ -1,8 +1,33 @@
+/*
+// DISPLAY JUST TEXT 01
 var svgContainer = d3.select("body")
-	.append("svg").attr("width","200").attr("height","200")
+	.append("svg")
+	.attr("width","200").attr("height","200")
 	.style('border', '1px solid black');
-	svgContainer.append("circle").attr("cx",100).attr("cy",100).attr("r",20).style("fill","black");
-
+	
 var svgText = svgContainer.append("text");
 	svgText.attr("x",50).attr("y",50);
 	svgText.text('circle')
+
+*/
+
+const dataSet = [
+	{"name":"Shanghai"        , "population":18, "rank": 1},
+	{"name":"Guangzhou"       , "population":11, "rank":10},
+	{"name":"Dongguan"        , "population": 8, "rank":20},
+	{"name":"Cairo"           , "population": 7, "rank":30},
+ 	{"name":"Saint Petersburg", "population": 5, "rank":40},
+	{"name":"New Taipei"      , "population": 4, "rank":50}
+];
+
+var svgContainer = d3.select("body")
+	.append("svg")
+	.attr("width","200").attr("height","200")
+	.style('border', '1px solid black');
+
+var circles = svgContainer.selectAll("circle")
+	.data(dataSet).enter().append("circle");
+
+	circles.attr("cx", (d,i) => (i + 1) * 25 )
+    .attr("cy", (d,i) => (i + 1) * 25 )
+    .attr("r" , (d,i) => d.population );
